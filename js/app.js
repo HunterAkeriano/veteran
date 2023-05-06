3a79eb727b2e027ea78cb18939925989967e196d
HunterAkeriano <stajectgamer@gmail.com>
2023-05-06 09:23:45 +0300

    Свайпер

--- js/app.js
+++ js/app.js
@@ -1,14 +1,56 @@
 const burgerMenu = document.querySelector('.menu');
-const menuInfo = document.querySelector('.menu-info')
+const menuInfo = document.querySelector('.menu-info');
+
 function openMenu(){
-    burgerMenu.classList.toggle('active')
+    let nav = burgerMenu.classList.toggle('active')
     menuInfo.classList.toggle('active');
-    if(burgerMenu){
+     if(nav){
         document.body.style.overflow = 'hidden';
     }
-    if(!burgerMenu){
-        document.body.style.overflow = 'scroll';
+    if(!nav){
+        document.body.style.overflowY = 'overlay';
     }
+   
+    console.log(burgerMenu)
+   
 }
 
+
 burgerMenu.addEventListener('click', openMenu);
+
+
+function closeMenu(){
+    burgerMenu.classList.remove('active')
+    menuInfo.classList.remove('active');
+}
+// закрытие
+const linkA = document.querySelectorAll('.link');
+linkA.forEach(item =>{
+    item.addEventListener('click', closeMenu)
+})
+
+// топ сладйер
+let swiper = new Swiper(".swiper1",{
+    spaceBetween: 10,
+ 
+    centerSlides: 'true',
+    fade: 'true',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+        1061: {
+            slidesPerView: 5,
+        },
+    }
+  });
